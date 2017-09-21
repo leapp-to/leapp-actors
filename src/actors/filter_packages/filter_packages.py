4f89d195280e77a8e60d9528a9c1597cf468c493
Vinzenz Feenstra <vfeenstr@redhat.com>
2017-09-21 23:35:02 +0200

    Apply pep8 checks

--- src/actors/filter_packages/filter_packages.py
+++ src/actors/filter_packages/filter_packages.py
@@ -1,7 +1,8 @@
 import json
 import sys
 data = json.load(sys.stdin)
-sys.stderr.write("\nREAD DATA: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" + json.dumps(data) + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+sys.stderr.write("\nREAD DATA: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                 json.dumps(data) + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 data = data["rpm_packages"]
 data["packages"] = [p for p in data["packages"] if p["name"].find(sys.argv[1]) != -1]
 print json.dumps({"filtered_packages": data})
