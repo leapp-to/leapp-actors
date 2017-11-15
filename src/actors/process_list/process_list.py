9fd89c6ae8b26c424d4e9220580460fb95977d2f
mfranczy <mfranczy@redhat.com>
2017-11-15 12:49:17 +0100

    fixed process_list

--- src/actors/process_list/process_list.py
+++ src/actors/process_list/process_list.py
@@ -9,24 +9,38 @@ def readlink(path, filename):
 
 def get_environ(pid):
     output = {}
-    with open(os.path.join(pid, 'environ'), 'rb') as f:
-        environ = f.read().split("\x00")
-        for env in environ:
-            if env:
-                name, val = env.split('=', 1)
-                output[name] = val
-    return output
+    try:
+        with open(os.path.join(pid, 'environ'), 'rb') as f:
+            environ = f.read().split("\x00")
+            for env in environ:
+                if env:
+                    name, val = env.split('=', 1)
+                    output[name] = val
+            return output
+    except EnvironmentError as exc:
+        sys.stderr.write(str(exc) + "\n")
 
 
 def get_cmdline(pid):
-    cmdline = None
-    with open(os.path.join(pid, 'cmdline'), 'rb') as f:
-        cmdline = f.read().replace("\x00", " ").strip()
-    return cmdline
+    try:
+        with open(os.path.join(pid, 'cmdline'), 'rb') as f:
+            return f.read().replace("\x00", " ").strip()
+    except EnvironmentError as exc:
+        sys.stderr.write(str(exc) + "\n")
+
+
+def extract_filter_param(params):
+    if params:
+        try:
+            return str(params['process_list_filter'][0]['value'])
+        except LookupError:
+            sys.stderr.write("Invalid filter param structure, continue without filtering...")
+    return ''
 
 
 if __name__ == '__main__':
     params = {} if sys.stdin.isatty() else json.load(sys.stdin)
+    filter_param = extract_filter_param(params)
 
     actor_output = {'process_list': [
         {'processes': []}
@@ -37,20 +51,17 @@ if __name__ == '__main__':
 
     for directory in os.listdir(root):
 
-        if not directory.isdigit():
-            continue
-        if directory == cpid:
+        if not directory.isdigit() or directory == cpid:
             continue
 
         pid = os.path.join(root, directory)
-        try:
-            cmdline = get_cmdline(pid)
-            environ = get_environ(pid)
-        except (OSError, IOError) as exc:
-            sys.stderr.write(str(exc) + "\n")
+        cmdline = get_cmdline(pid)
+        environ = get_environ(pid)
+
+        if cmdline is None or environ is None:
             continue
 
-        if params is not None and not str(params.get('filter', '')) in cmdline:
+        if filter_param not in cmdline:
             continue
 
         actor_output['process_list'][0]['processes'].append({
