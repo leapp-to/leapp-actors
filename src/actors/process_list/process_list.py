d6388bdaa3cf3716bf095ce105b9c10c392929c4
mfranczy <mfranczy@redhat.com>
2017-11-15 09:33:41 +0100

    process_list - refactoring

--- src/actors/process_list/process_list.py
+++ src/actors/process_list/process_list.py
@@ -2,49 +2,62 @@ import os
 import sys
 import json
 
+
 def readlink(path, filename):
     return os.readlink(os.path.join(path, filename))
 
-output = {'process_list': [
-    {'processes': []}
-]}
 
-root = '/proc'
-pid = str(os.getpid())
+def get_environ(pid):
+    output = {}
+    with open(os.path.join(pid, 'environ'), 'rb') as f:
+        environ = f.read().split("\x00")
+        for env in environ:
+            if env:
+                name, val = env.split('=', 1)
+                output[name] = val
+    return output
 
-params = {}
-if not sys.stdin.isatty():
-    params = json.load(sys.stdin)
 
-for directory in os.listdir(root):
-    if not directory.isdigit():
-        continue
-    if directory == pid:
-        continue
+def get_cmdline(pid):
+    cmdline = None
+    with open(os.path.join(pid, 'cmdline'), 'rb') as f:
+        cmdline = f.read().replace("\x00", " ").strip()
+    return cmdline
 
-    proc_dir = os.path.join(root, directory)
-    try:
-        with open(os.path.join(proc_dir, 'cmdline'), 'rb') as f:
-            cmdline = f.read().replace("\x00", " ").strip()
 
-        with open(os.path.join(proc_dir, 'environ'), 'rb') as f:
-            environ_output = {}
-            environ = f.read().split("\x00")
-            for env in environ:
-                if env:
-                    name, val = env.split('=', 1)
-                    environ_output[name] = val
+if __name__ == '__main__':
+    params = {} if sys.stdin.isatty() else json.load(sys.stdin)
+
+    actor_output = {'process_list': [
+        {'processes': []}
+    ]}
+
+    root = '/proc'
+    cpid = str(os.getpid())
+
+    for directory in os.listdir(root):
+
+        if not directory.isdigit():
+            continue
+        if directory == cpid:
+            continue
+
+        pid = os.path.join(root, directory)
+        try:
+            cmdline = get_cmdline(pid)
+            environ = get_environ(pid)
+        except (OSError, IOError) as exc:
+            sys.stderr.write(str(exc) + "\n")
+            continue
 
         if params is not None and not str(params.get('filter', '')) in cmdline:
             continue
 
-        output['process_list'][0]['processes'].append({
-            "cwd": readlink(proc_dir, "cwd"),
-            "exe": readlink(proc_dir, "exe"),
-            "environ": environ_output,
+        actor_output['process_list'][0]['processes'].append({
+            "cwd": readlink(pid, "cwd"),
+            "exe": readlink(pid, "exe"),
+            "environ": environ,
             "cmdline": cmdline
         })
-    except (OSError, IOError) as exc:
-        sys.stderr.write(str(exc) + "\n")
 
-sys.stdout.write(json.dumps(output))
+    sys.stdout.write(json.dumps(actor_output))
