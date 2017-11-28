35affa1941bcc990872b6b6b821fc5cfb8763b5e
Arthur Mello <amello@redhat.com>
2017-11-28 22:21:05 +0000

    Organized schema folder
    - Renamed folder to "schemas"
    - Removed unused schemas (TypeBaseNumber is not used but was not removed since it is a basic schema)
    - Removed "String" schema and used existent "BaseTypeString" instead
    - Modified "list_path_content" and "list_content_pkg" actors to use a string list that do not contain duplicates
    - Removed "StringList" schema and used existent "TypeStringList" instead

--- src/schema/string.py
+++
@@ -1,8 +0,0 @@
-from jsl import Document
-from jsl.fields import StringField
-from snactor.registry.schemas import registered_schema
-
-
-@registered_schema('1.0')
-class String(Document):
-    value = StringField()
