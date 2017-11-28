442c07480ce9b553d34e483d1b572a714cf9da34
Tomas Meszaros <exo@tty.sk>
2017-11-28 15:54:23 +0100

    Apache scanner v2
    Removed some code. It should be more readable.
    
    I will add documentation and additional changes in v3.
    Also, I will test this on some bigger apache deployment but don't
    have time right now. In the meanwhile, please write review and
    I will make final v3 next time.

--- src/actors/apache_scanner/apache_scanner.py
+++ src/actors/apache_scanner/apache_scanner.py
@@ -4,8 +4,8 @@
 Author: tmeszaro@redhat.com
 
 Actor that extracts selected data from the Augeas actor output.
-If you want to extract additional data from the augeas aug_httpd lens, check
-'searches' tuple in the printall() function.
+If you want to extract additional data from the augeas aug_httpd lens,
+specify them in the 'SEARCHES' tuple.
 
 
 Possible improvements:
@@ -18,109 +18,51 @@ Possible improvements:
 import sys
 import json
 
+SEARCHES = (
+    ("IncludeOptional",),
+    ("User",),
+    ("Group",),
+    ("Mutex",),
+    ("PidFile",),
+    ("Listen",),
+    ("TypesConfig",),
+    ("CacheRoot",),
+    ("MIMEMagicFile",),
+    ("ScriptSock",),
+    ("SSLCertificateKeyFile",),
+    ("SSLCertificateFile",),
+    ("CustomLog",),
+    ("ErrorLog",),
+    ("Include",),
+    ("VirtualHost",),
+    ("Directory",),
+    ("DocumentRoot",),
+    ("absolute_path",),
+    ("LoadModule", 1),
+)
 
-def searchblock(tablelist, mykey, myvalue=None, block=True):
-    """ Searches augeas output for specified key or key-value and yields
-        corresponding matched data.
 
-        Args:
-            tablelist: 'aug_httpd' lens data from the augeas actor output
-            mykey: key of the block we want to search for
-            myvalue: value of the block we want to search for,
-                     if left None then get everything that matches at least mykey
-            block: if False, do not yield whole dict but only key-value pair
-
-        Returns:
-            Yields the whole matching subdict of the tablelist if block=True
-            or only single key-value pair otherwise.
-    """
+def extract(augeas_output):
     # Sometimes augeas gives us list of dicts and sometimes only dict.
     # We want to always have the data in the list.
-    if isinstance(tablelist, dict):
-        tablelist = [tablelist]
+    if isinstance(augeas_output, dict):
+        augeas_output = [augeas_output]
 
-    for table in tablelist:
-        for key in table:
-            if (myvalue is None and key == mykey) or \
-               (myvalue is not None and key == mykey and myvalue == table[key]):
-                if block is True:
-                    yield table
-                else:
-                    yield key + "=" + table[key]
-        if 'properties' in table:
-            for generator in searchblock(table['properties'], mykey, myvalue, block):
-                yield generator
-
-
-def getblock(tablelist, mykey, myvalue=None, block=True, propitem=0):
-    """ Accumulates matched data and returns them for output.
-
-        Arguments:
-            block: if False, will pass block=False to searchblock
-            propitem: specify from which index in 'properties' to search
-
-        Returns:
-            Extracted data in form of dictionary.
-    """
+    def search(augeas_data, item, propindex=0):
+        for table in augeas_data:
+            for key in table:
+                if item == key:
+                    yield (item, table[key])
+                if item == table[key] and 'properties' in table:
+                    if 'value' in table['properties'][propindex]:
+                        yield (item, table['properties'][propindex]['value'])
+            if 'properties' in table:
+                for generator in search(table['properties'], item, propindex):
+                    yield generator
     out = {}
-    if block is True:
-        for dic in searchblock(tablelist, mykey, myvalue, block):
-            if dic[mykey] not in out:
-                out[dic[mykey]] = [dic['properties'][propitem]['value']]
-            else:
-                out[dic[mykey]].append(dic['properties'][propitem]['value'])
-    else:
-        for line in searchblock(tablelist, mykey, myvalue, block):
-            key, value = line.split("=")
-            if key not in out:
-                out[key] = [value]
-            else:
-                out[key].append(value)
-    return out
-
-
-def printall(tablelist):
-    """ Prints all occurrences of the specified key-value pairs.
-
-    Tuple @searches contains information about what to extract and how it will
-    be extracted (see @block and propitem@ arguments to getblock() function.
-
-    Args:
-        tablelist: 'aug_httpd' lens data from the augeas actor output
-
-    Returns:
-        json formatted extracted data from the augeas aug_httpd lens
-    """
-    searches = (
-        # Arguments for the getblock function.
-        # mykey, myvalue=None, block=True, propitem=0
-        ("value", "IncludeOptional"),
-        ("value", "User"),
-        ("value", "Group"),
-        ("value", "Mutex"),
-        ("value", "PidFile"),
-        ("value", "Listen"),
-        ("value", "TypesConfig"),
-        ("value", "CacheRoot"),
-        ("value", "MIMEMagicFile"),
-        ("value", "ScriptSock"),
-        ("value", "SSLCertificateKeyFile"),
-        ("value", "SSLCertificateFile"),
-        ("value", "CustomLog"),
-        ("value", "ErrorLog"),
-        ("value", "Include"),
-        ("name", "VirtualHost"),
-        ("name", "Directory"),
-        ("value", "DocumentRoot"),
-        ("absolute_path", None, False),
-        ("value", "LoadModule", True, 1),
-    )
-
-    out = {}
-    for args in searches:
-        result_table = getblock(tablelist, *args)
-        for key in result_table:
-            out[key] = result_table[key]
+    for args in SEARCHES:
+        for result in search(augeas_output, *args):
+            out.setdefault(result[0], []).append(result[1])
     return {"apache_scanner": [out]}
 
 
@@ -128,10 +70,11 @@ if __name__ == "__main__":
     AUGEAS_OUTPUT = json.load(sys.stdin)
     try:
         # We are interested only in the aug_httpd lens from the augeas actor
-        #  output. We can drop every other augeas lens.
-        TABLELIST = AUGEAS_OUTPUT['aug_httpd']
-    except KeyError:
-        raise KeyError("Could not get lens 'aug_httpd' from the augeas actor " \
-                        "output. Make sure augeas actor is producing data in " \
-                        "aug_httpd lens.")
-    print(json.dumps(printall(TABLELIST)))
+        # output. We can drop every other augeas lens.
+        AUG_HTTPD = AUGEAS_OUTPUT['aug_httpd']
+    except KeyError as error:
+        error.args = ("Could not get lens 'aug_httpd' from the augeas actor "
+                      + "output. Make sure augeas actor is producing data in "
+                      + "the aug_httpd lens.",)
+        raise
+    print(json.dumps(extract(AUG_HTTPD)))
