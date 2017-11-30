ffc442668cc51a1f2ea4741f8007f3bee7a48c66
Arthur Mello <amello@redhat.com>
2017-11-30 15:04:34 +0000

    Rename basic type schemas
    - Renamed TypeBaseBool to Bool
    - Renamed TypeBaseNumber to Number
    - Renamed TypeBaseString to String
    - Renamed TypeStringList to StringList

--- src/schemas/bool.py
+++ src/schemas/bool.py
@@ -1,8 +1,8 @@
 from jsl import Document
-from jsl.fields import NumberField
+from jsl.fields import BooleanField
 from snactor.registry.schemas import registered_schema
 
 
 @registered_schema('1.0')
-class BaseTypeNumber(Document):
-    value = NumberField()
+class Bool(Document):
+    value = BooleanField()
