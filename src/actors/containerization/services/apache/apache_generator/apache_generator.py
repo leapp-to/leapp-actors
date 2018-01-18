583d1583b66481cbcd30fe0bc21df041783cd518
Tomas Meszaros <exo@tty.sk>
2018-01-18 13:53:02 +0100

    apache_generator: dropped dnf & cleaned up the code

--- src/actors/containerization/services/apache/apache_generator/apache_generator.py
+++ src/actors/containerization/services/apache/apache_generator/apache_generator.py
@@ -4,15 +4,15 @@
 Author: tmeszaro@redhat.com
 
 
-Apache generator actor. Uses data from apache_scanner, creates image with buildah
-and pushes this image into the specified registrty.
+Apache generator actor. Uses data from apache_scanner, creates image with
+buildah and pushes this image into the specified registrty.
 
 
 Running the container:
 
 - after pulling image from registry, run:
 
-$ docker run -dt -p HOST_PORT:CONTAINER_PORT IMAGE_ID
+$ docker run -dt -p HOST_port:CONTAINER_port IMAGE_ID
 
 
 Possible improvements:
@@ -33,8 +33,6 @@ import subprocess
 import shlex
 from distutils.spawn import find_executable
 
-import dnf
-
 
 def find_buildah():
     def _is_valid(path):
@@ -43,68 +41,51 @@ def find_buildah():
     if not _is_valid(path):
         path = find_executable('buildah')
     if not _is_valid(path):
-        raise Exception('Could not find a valid buildah binary: {0}'.format(path))
+        raise Exception('Could not find a valid buildah binary: %s' % path)
     return path
+
 
 BUILDAH = find_buildah()
 
-def execute(cmd):
-    out = subprocess.check_output(shlex.split(cmd))
-    return out.strip()
 
-def create_httpd_container():
-    cname = execute('{buildah} from fedora'.format(buildah=BUILDAH))
+def execute(cmd):
+   try:
+       return subprocess.check_output(shlex.split(cmd))
+   except subprocess.CalledProcessError as e:
+       sys.stderr.write(str(e) + '\n')
+       return None
+
+
+def create_httpd_container(image):
+    cname = execute('%s from %s' % (BUILDAH, image))
     if not cname:
         raise Exception('Error creating container')
     return cname
 
+
 def copy_to_container(cname, src, dst):
     if cname and src and dst:
-        execute('{buildah} copy {cname} {src} {dst}'.format(
-            buildah=BUILDAH,
-            cname=cname,
-            src=src,
-            dst=dst
-        ))
+        execute('%s copy %s %s %s' % (BUILDAH, cname, src, dst))
 
-def get_container_id(cname):
-    cntrs = json.loads(execute('{buildah} containers --json'.format(buildah=BUILDAH)))
-    ids = [x['id'].strip() for x in cntrs if x['containername'] == cname]
-    if len(ids) != 1:
-        raise Exception('Incorrect number of containers with name {0}'.format(cname))
-    return ids[0]
 
 if __name__ == "__main__":
     INPUT = json.load(sys.stdin)
-
     APACHE_SCANNER = INPUT['apache_scanner'][0]
-    REGISTRY = INPUT['registry'][0]
-    USER = REGISTRY['user']
-    PW = REGISTRY['password']
-    SOURCE_IMG_NAME = "registry.fedoraproject.org/fedora:26"
-    TARGET_REGISTRY = REGISTRY['address']
 
-    cname = execute('%s from %s' % (BUILDAH, SOURCE_IMG_NAME))
+    registry = INPUT['registry'][0]
+    user = registry['user']
+    password = registry['password']
 
-    FINAL = TARGET_REGISTRY + "/" + USER + "/" + cname
+    cname = create_httpd_container(image="registry.fedoraproject.org/fedora:26")
+    image_uri = registry['address'] + "/" + user + "/" + cname
 
-    execute('%s run %s -- dnf update -y' % (BUILDAH, cname))
-    execute('%s run %s -- dnf install -y httpd' % (BUILDAH, cname))
+    execute('%s run %s -- yum update -y' % (BUILDAH, cname))
+    execute('%s run %s -- yum install -y httpd' % (BUILDAH, cname))
 
     ## Install missing modules.
-    base = dnf.Base()
-    base.fill_sack()
-    QUERY = base.sack.query()
     for module_path in APACHE_SCANNER["LoadModule"]:
-        ## Getting httpd module name from the module file name is probably
-        ##  not 100% reliable way to get package name (but it seems to work).
-        ## Using "rpm provides" is probaby more reliable, but it requires to
-        ##  parse the rpm output (which can be messy).
-        pkg_name = "*%s*" % module_path.split('/')[1].split('.')[0]
-        result = QUERY.filter(name__glob=pkg_name).run()
-        for pkg in result:
-            if not pkg.installed:
-                execute("%s run %s -- dnf install -y %s" % (BUILDAH, cname, pkg.name))
+        pkg_name = module_path.split('/')[-1].split('.so')[0]
+        execute("%s run %s -- yum install -y %s" % (BUILDAH, cname, pkg_name))
 
     ## Copy configs & data to image.
     for config in APACHE_SCANNER["absolute_path"]:
@@ -115,11 +96,9 @@ if __name__ == "__main__":
     ## Right now, we only run httpd with default httpd.conf location.
     execute('%s config %s --cmd "httpd -DFOREGROUND"' % (BUILDAH, cname))
 
-    PORT = APACHE_SCANNER['Listen'][0]
-    execute('%s config %s --port %s' % (BUILDAH, cname, PORT))
-    execute('%s commit %s %s' % (BUILDAH, cname, FINAL))
-    execute('%s push --creds=%s:%s %s %s' % (BUILDAH, USER, PW, FINAL, FINAL))
+    port = APACHE_SCANNER['Listen'][0]
+    execute('%s config %s --port %s' % (BUILDAH, cname, port))
+    execute('%s commit %s %s' % (BUILDAH, cname, image_uri))
+    execute('%s push --creds=%s:%s %s %s' % (BUILDAH, user, password, image_uri, image_uri))
 
-    print(json.dumps({'apache_generator': [{'image': cname,
-                                            'uri': FINAL,
-                                            'port': PORT}]}))
+    print(json.dumps({'apache_generator': [{'image': cname, 'uri': image_uri, 'port': port}]}))
