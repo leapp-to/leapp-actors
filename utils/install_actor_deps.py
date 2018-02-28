7cabfcb7b0710fccb016758cbeffc11ae9377d7d
Jozef Mikovic <jmikovic@redhat.com>
2018-02-28 15:24:47 +0100

    add actors directory variable

--- utils/install_actor_deps.py
+++ utils/install_actor_deps.py
@@ -29,8 +29,8 @@ def install(path):
         error(str(e)+'\n', e.returncode)
 
 
-def install_actor_deps(actor):
-    for root, dirs, files in os.walk('./src/actors'):
+def install_actor_deps(actor, directory):
+    for root, dirs, files in os.walk(directory):
         if actor in dirs:
             makefile_path = os.path.join(root, actor, 'Makefile')
             if os.path.isfile(makefile_path):
@@ -40,18 +40,20 @@ def install_actor_deps(actor):
     error("Actor '{}' doesn't exist!\n".format(actor), 1)
 
 
-def install_all_deps():
-    for root, dirs, files in os.walk('./src/actors'):
+def install_all_deps(directory):
+    for root, dirs, files in os.walk(directory):
         if 'Makefile' in files:
             install(os.path.join(root, 'Makefile'))
 
 
 if __name__ == "__main__":
+    ACTORS_DIR = './src/actors'
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--actor", help="name of the actor for which to install dependencies")
     args = parser.parse_args()
 
     if args.actor:
-        install_actor_deps(args.actor)
+        install_actor_deps(args.actor, ACTORS_DIR)
     else:
-        install_all_deps()
+        install_all_deps(ACTORS_DIR)
